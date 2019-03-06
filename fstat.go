53a894ff81ff11b32fd5845a691bc1729970fb40
John Taylor <johntaylor@gmail.com>
2019-03-06 11:28:30 -0500

    added introduction, license, updated TODO list

--- fstat.go
+++ fstat.go
@@ -1,9 +1,26 @@
 
+/*
+
+fstat.go
+-John Taylor
+Mar 2019
+
+Get info for a list of files across multiple directories
+
+To compile:
+go build -ldflags="-s -w" fstat.go
+
+MIT License; Copyright (c) 2018 John Taylor
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
 /* TODO
 
--v version 
 -q quiet, suppress errors
-use chanels to improve performance
+possibly use go channels to improve performance
 append optional summary at end in separate table
 add commas to file sizes
 convert file sizes to MB
@@ -26,7 +43,7 @@ import (
     "github.com/olekukonko/tablewriter"
 )
 
-const version = "1.0.0"
+const version = "1.0.1"
 
 type FileStat struct {
     Name string
