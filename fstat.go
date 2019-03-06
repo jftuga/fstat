ce634a608b0be705ae464b5f25e09137092e0821
John Taylor <johntaylor@gmail.com>
2019-03-06 05:34:17 -0500

    work in progress

--- fstat.go
+++ fstat.go
@@ -6,32 +6,159 @@ import (
     "fmt"
     "flag"
     "os"
+    "sort"
+    "strings"
+    "time"
 
     "github.com/olekukonko/tablewriter"
 )
 
-func GetFileInfo(input *bufio.Scanner) {
-    var allRows [][]string
+type FileStat struct {
+    Name string
+    FullName string
+    Size int64
+    ModTime time.Time
+    FileType string
+}
+
+
+func sortSize(entry []FileStat, ascending bool) {
+    sort.Slice(entry, func(i, j int) bool {
+        if entry[i].Size > entry[j].Size {
+            return !ascending
+        }
+        if entry[i].Size < entry[j].Size {
+            return ascending
+        }
+        // when multiple lines have the same Size, then alphabetize these lines
+        return entry[i].Name < entry[j].Name
+    })
+}
+
+func sortModTime(entry []FileStat, ascending bool) {
+    sort.Slice(entry, func(i, j int) bool {
+        if entry[i].ModTime.After(entry[j].ModTime) {
+            return !ascending
+        }
+        if entry[i].ModTime.Before(entry[j].ModTime) {
+            return ascending
+        }
+        // when multiple lines have the same Size, then alphabetize these lines
+        return entry[i].Name < entry[j].Name
+    })
+}
+
+func sortName(entry []FileStat, ascending bool) {
+    sort.Slice(entry, func(i, j int) bool {
+        if ascending {
+            return entry[i].FullName < entry[j].FullName
+        } else {
+            return entry[i].FullName > entry[j].FullName
+        }
+    })
+}
+
+func sortNameCaseInsensitive(entry []FileStat, ascending bool) {
+    sort.Slice(entry, func(i, j int) bool {
+        if ascending {
+            return strings.ToLower(entry[i].FullName) < strings.ToLower(entry[j].FullName)
+        } else {
+            return strings.ToLower(entry[i].FullName) > strings.ToLower(entry[j].FullName)
+        }
+    })
+}
+
+func GetFileInfo(input *bufio.Scanner) ([]FileStat) {
+    var allEntries []FileStat
     fname := ""
 
     for input.Scan() {
         fname = input.Text()
-        f,_ := os.Stat(fname)
+        f,err := os.Lstat(fname)
 
-        fmt.Printf("%s\t%d\t%s\n", f.ModTime(), f.Size(), f.Name())
-        row := []string{fmt.Sprintf("%s",f.ModTime())[:19], fmt.Sprintf("%d",f.Size()), fname}
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+            continue
+        }
+
+        var ftype string = "?"
+        if f.Mode().IsRegular() {
+            ftype = "F"
+        } else if f.IsDir() {
+            ftype = "D"
+        } else if f.Mode() & os.ModeSymlink == os.ModeSymlink {
+            ftype = "L"
+        }
+
+        entry := FileStat{Name: f.Name(), FullName: fname, Size: f.Size(), ModTime: f.ModTime(), FileType: ftype}
+        allEntries = append(allEntries, entry)
+    }
+    return allEntries
+}
+
+func RenderAllEntries(allEntries []FileStat) {
+
+    var allRows [][]string
+    var e FileStat
+    for _,e = range allEntries {
+        row := []string{fmt.Sprintf("%s",e.ModTime)[:19], fmt.Sprintf("%d",e.Size), fmt.Sprintf("%s",e.FileType), e.FullName}
         allRows = append(allRows, row)
     }
 
     table := tablewriter.NewWriter(os.Stdout)
     table.SetAutoWrapText(false)
-    table.SetHeader([]string{"Mod Time","Size","Name"})
+    table.SetHeader([]string{"Mod Time","Size","Type","Name"})
     table.AppendBulk(allRows)
-    table.Render()
+    if len(allRows) > 0 {
+        table.Render()
+    }
+}
+
+func ValidateArgs(argsSortSize *bool, argsSortSizeDesc *bool, argsSortModTime *bool, argsSortModTimeDesc *bool, argsSortName *bool, argsSortNameDesc *bool, argsSortNameCaseInsen *bool, argsSortNameCaseInsenDesc *bool ) {
+
+    count := 0
+    if *argsSortSize { count++ }
+    if *argsSortSizeDesc { count++ }
+    if *argsSortModTime { count++ }
+    if *argsSortModTimeDesc { count++ }
+    if *argsSortName { count++ }
+    if *argsSortNameDesc { count++ }
+    if *argsSortNameCaseInsen { count++ }
+    if *argsSortNameCaseInsenDesc { count++ }
+
+    if count > 1 {
+        fmt.Fprintf(os.Stderr,"Error: only one sorting argument can be given.\n\n")
+        os.Exit(2)
+    }
+}
+
+func SortAllEntries(allEntries []FileStat, argsSortSize *bool, argsSortSizeDesc *bool, argsSortModTime *bool, argsSortModTimeDesc *bool, argsSortName *bool, argsSortNameDesc *bool, argsSortNameCaseInsen *bool, argsSortNameCaseInsenDesc *bool ) {
+
+    if *argsSortSize { sortSize(allEntries,true) }
+    if *argsSortSizeDesc { sortSize(allEntries,false) }
+    if *argsSortModTime { sortModTime(allEntries,true) }
+    if *argsSortModTimeDesc { sortModTime(allEntries,false) }
+    if *argsSortName { sortName(allEntries,true) }
+    if *argsSortNameDesc { sortName(allEntries,false) }
+    if *argsSortNameCaseInsen { sortNameCaseInsensitive(allEntries,true) }
+    if *argsSortNameCaseInsenDesc { sortNameCaseInsensitive(allEntries,false) }
 }
 
 func main() {
+    argsSortSize := flag.Bool("s", false, "sort by file size")
+    argsSortSizeDesc := flag.Bool("S", false, "sort by file size, descending")
+
+    argsSortModTime := flag.Bool("d", false, "sort by file modified date")
+    argsSortModTimeDesc := flag.Bool("D", false, "sort by file modified date, reverse chronologically")
+
+    argsSortName := flag.Bool("n", false, "sort by file name")
+    argsSortNameDesc := flag.Bool("N", false, "sort by file name, reverse alphabetically")
+
+    argsSortNameCaseInsen := flag.Bool("c", false, "case-insensitive sort by file name")
+    argsSortNameCaseInsenDesc := flag.Bool("C", false, "case-insensitive sort by file name, reverse alphabetically")
+
     flag.Parse()
+    ValidateArgs(argsSortSize, argsSortSizeDesc, argsSortModTime, argsSortModTimeDesc, argsSortName, argsSortNameDesc, argsSortNameCaseInsen, argsSortNameCaseInsenDesc)
     args := flag.Args()
 
     var input *bufio.Scanner
@@ -48,6 +175,8 @@ func main() {
         input = bufio.NewScanner(file)
     }
 
-    GetFileInfo(input)
+    allEntries := GetFileInfo(input)
+    SortAllEntries(allEntries,argsSortSize, argsSortSizeDesc, argsSortModTime, argsSortModTimeDesc, argsSortName, argsSortNameDesc, argsSortNameCaseInsen, argsSortNameCaseInsenDesc)
+    RenderAllEntries(allEntries)
 }
 
